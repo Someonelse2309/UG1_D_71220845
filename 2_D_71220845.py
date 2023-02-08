413fa25edd90a9894a989bfad77c9b92b4b7bec8
Someonelse2309 <117884155+Someonelse2309@users.noreply.github.com>
2023-02-08 18:55:05 +0700

    Add files via upload

--- 2_D_71220845.py
+++ 2_D_71220845.py
@@ -2,4 +2,7 @@ kal = str(input("Masukan Kalimat Anda\n>> "))
 kat = str(input("Kata Yang Ingin Dicari\n>> "))
 kal1 = kal.lower()
 kat1 = kal1.count(kat)
-print (kat1)
+if kat1 == 0:
+    print("Maaf Tidak Ada Kata Yang Dicari")
+else:
+    print (kat1)
